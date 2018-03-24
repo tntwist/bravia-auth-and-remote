09e3b1b8602aa9bba455a0ead421fcbaa98fb06c
Nico Lachmuth <nico92_7@hotmail.de>
2018-03-24 16:00:47 +0100

    AddedPSK as Argument

--- send_command.sh
+++ send_command.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-if [ "$1" = "" ] || [ "$2" = "" ]; then
-  echo "Usage: $0 <TV_IP> <IRCC_COMMAND>"
+if [ "$1" = "" ] || [ "$2" = "" ] || ["$3" = ""]; then
+  echo "Usage of Sony Bravia IRCC Sender: $0 TV_IP IRCC_COMMAND X-AUTH-PSK"
   exit 1
 fi
 
 cmd="<?xml version=\"1.0\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:X_SendIRCC xmlns:u=\"urn:schemas-sony-com:service:IRCC:1\"><IRCCCode>$2</IRCCCode></u:X_SendIRCC></s:Body></s:Envelope>"
 
 
-code=$(curl -w "%{http_code}" --silent -XPOST http://$1/sony/IRCC -d "$cmd" -H 'Content-Type: text/xml; charset=UTF-8' -H 'SOAPACTION: "urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"' -H "X-Auth-PSK: 0000" -o /dev/null )
+code=$(curl -w "%{http_code}" --silent -XPOST http://$1/sony/IRCC -d "$cmd" -H 'Content-Type: text/xml; charset=UTF-8' -H 'SOAPACTION: "urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"' -H "X-Auth-PSK: $3" -o /dev/null )
 
 if [ "$code" = "200" ]; then
   echo "✓"
